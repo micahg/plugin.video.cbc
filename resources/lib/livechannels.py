38d15b69806dc376b6d55d57ac4ba39c1c30c338
Micah Galizia <micahgalizia@gmail.com>
2021-08-23 21:50:32 -0400

    code cleanup and title cleanup

--- resources/lib/livechannels.py
+++ resources/lib/livechannels.py
@@ -4,56 +4,59 @@ from urllib.parse import urlencode
 
 import requests
 
-from .utils import saveCookies, loadCookies, log
+from resources.lib.utils import saveCookies, loadCookies, log
 from resources.lib.cbc import CBC
+
+LIST_URL = 'https://tpfeed.cbc.ca/f/ExhSPC/t_t3UKJR6MAT?pretty=true&sort=pubDate%7Cdesc'
+LIST_ELEMENT = 'entries'
 
 
 class LiveChannels:
     """Class for live channels."""
+
     def __init__(self):
         """Initialize the live channels class."""
-        self.LIST_URL = 'http://tpfeed.cbc.ca/f/ExhSPC/t_t3UKJR6MAT?pretty=true&sort=pubDate%7Cdesc'
-        self.LIST_ELEMENT = 'entries'
-
         # Create requests session object
         self.session = requests.Session()
         session_cookies = loadCookies()
-        if not session_cookies == None:
+        if session_cookies is not None:
             self.session.cookies = session_cookies
 
-
     def get_live_channels(self):
-        r = self.session.get(self.LIST_URL)
+        """Get the list of live channels."""
+        resp = self.session.get(LIST_URL)
 
-        if not r.status_code == 200:
-            log('ERROR: {} returns status of {}'.format(url, r.status_code), True)
+        if not resp.status_code == 200:
+            log('ERROR: {} returns status of {}'.format(LIST_URL, resp.status_code), True)
             return None
         saveCookies(self.session.cookies)
 
-        items = json.loads(r.content)[self.LIST_ELEMENT]
+        items = json.loads(resp.content)[LIST_ELEMENT]
         return items
 
     def get_iptv_channels(self):
         """Get the channels in a IPTV Manager compatible list."""
         cbc = CBC()
         channels = self.get_live_channels()
-        # [ {'name': channel.name, 'stream': channel.} for channel in channels ]
         result = []
-        log('MICAH IPTV REQUEST FOR CHANNELS IS {}'.format(channels), True)
         for channel in channels:
-            log('MICAH CHANNEL IS {}'.format(channel), True)
             labels = cbc.getLabels(channel)
+            image = cbc.getImage(channel)
             values = {
                 'url': channel['content'][0]['url'],
-                'image': channel['cbc$staticImage'],
+                'image': image,
                 'labels': urlencode(labels)
             }
             channel_dict = {
                 'name': channel['title'],
                 'stream': 'plugin://plugin.video.cbc/smil?' + urlencode(values),
                 'id': channel['cbc$callSign'],
-                'logo': channel['cbc$staticImage']
+                'logo': image
             }
+
+            # Use "CBC Toronto" instead of "Toronto"
+            if len(channel_dict['name']) < 4 or channel_dict['name'][0:4] != 'CBC ':
+                channel_dict['name'] = 'CBC {}'.format(channel_dict['name'])
             result.append(channel_dict)
-        log('MICAH IPTV RETURNING {}'.format(result), True)
+
         return result
